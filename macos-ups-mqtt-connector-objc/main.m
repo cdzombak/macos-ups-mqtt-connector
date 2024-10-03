0c9e813e6d6c4fa6569d8f3927460150fe6459b7
Chris Dzombak <chris@dzombak.com>
2024-10-03 10:07:43 -0400

    Initial Commit

---
+++ macos-ups-mqtt-connector-objc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  macos-ups-mqtt-connector-objc
+//
+//  Created by Chris Dzombak on 10/3/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
